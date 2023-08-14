c916d9c7e7a7034ebe1301f4fa092b61d17a784b
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-08-14 17:59:22 +0300

    add loading.js file & cutom spinner

---
+++ app/loading.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from './styles.module.css'
+
+const Loading = () => {
+  return (
+    <div className={styles.customSpinner}>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+      <div></div>
+    </div>
+  );
+};
+
+export default Loading;
