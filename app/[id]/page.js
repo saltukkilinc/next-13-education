656e668c82da1e32864ac98ee6975d469a132a86
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-08-14 18:47:49 +0300

    add error page

--- app/[id]/page.js
+++ app/[id]/page.js
@@ -1,34 +1,42 @@
-import Image from 'next/image';
-import {notFound} from 'next/navigation'
-import React from 'react'
+import Image from "next/image";
+import { notFound } from "next/navigation";
+import React from "react";
 
-import styles from './styles.module.css'
+import styles from "./styles.module.css";
 
 const fetchIndividualPost = async (id) => {
- const res = await fetch(`https://dummyjson.com/post/${id}`);
- return await res.json();
-}
+  try {
+    const res = await fetch(`https://dummyjson.com/post/${id}`);
+    return await res.json();
+  } catch (error) {
+    throw new Error(error);
+  }
+};
 
-const Post = async ({params}) => {
+const Post = async ({ params }) => {
   const fetchedData = await fetchIndividualPost(params.id);
 
   // console.log(fetchedData)
 
-  const {title, body} = fetchedData
+  const { title, body } = fetchedData;
 
-  if(!title) {
-    notFound()
+  if (!title) {
+    notFound();
   }
-  
+
   return (
     <div className={styles.container}>
       <div className={styles.imageContainer}>
-        <Image src={`https://picsum.photos/200/300?random=${params.id}`} alt="lorem picsum image" fill/>
+        <Image
+          src={`https://picsum.photos/200/300?random=${params.id}`}
+          alt="lorem picsum image"
+          fill
+        />
       </div>
       <h3 className={styles.title}>{title}</h3>
       <p>{body}</p>
     </div>
-  )
-}
+  );
+};
 
-export default Post
+export default Post;
---
+++ app/error.js
@@ -0,0 +1,13 @@
+"use client";
+import React from 'react'
+
+const Error = () => {
+  return (
+    <div>
+      <h1>Error Page</h1>
+      <p>There is an Error in your code.This is not he same with not found.To use this, next js cath an Error in your code.</p>
+    </div>
+  )
+}
+
+export default Error
