7132f530344714224d9ebbb1d9949b939f981443
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-08-14 13:49:19 +0300

    add metadata object for title

--- app/layout.js
+++ app/layout.js
@@ -1,9 +1,31 @@
+import "../styles/global.css";
+import styles from "./styles.module.css";
+import { Inter } from 'next/font/google'
 
+ 
+export const metadata = {
+  title: 'Next.js 13 | Blog',
+}
+ 
+ 
+// If loading a variable font, you don't need to specify the font weight
+const inter = Inter({
+  subsets: ['latin'],
+  display: 'swap',
+})
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body >{children}</body>
+    <html lang="en" className={inter.className}>
+      <body className={styles.container}>
+        <header>
+          <p>Header</p>
+        </header>
+        {children}
+        <footer>
+          <p>Footer</p>
+        </footer>
+      </body>
     </html>
-  )
+  );
 }
--- app/page.js
+++ app/page.js
@@ -1,8 +1,8 @@
 
 export default function Home() {
   return (
-    <div>
-      Home Page
-    </div>
+    <main>
+      <p>Home Page</p>
+    </main>
   )
 }
