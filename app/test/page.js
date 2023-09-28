e01a0449f1b99e83a310b9f50bc6e4041859e411
saltukkilinc <112329706+saltukkilinc@users.noreply.github.com>
2023-09-28 21:41:19 +0300

    test route

---
+++ app/test/page.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Hello = () => {
+  return (
+    <div style={{color: "white"}}>
+      You should add page.js inside the folder to add a route.
+    </div>
+  )
+}
+
+export default Hello
